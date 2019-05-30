7fd0324213b3e5f53cfb7b12b05d0e9f9ce1597d
Matt D <dwinell.matt@gmail.com>
2019-05-30 11:24:08 -0400

    began working on firebase authentication

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var firebaseConfig = {
         apiKey: "AIzaSyCfx7JuJ4eEZIpbbgNmvNPTOkPUDuIVyyo",
         authDomain: "dndcharcreator-2c7a2.firebaseapp.com",
@@ -7,37 +7,61 @@ $(document).ready(function(){
         storageBucket: "dndcharcreator-2c7a2.appspot.com",
         messagingSenderId: "427842856156",
         appId: "1:427842856156:web:71475bda7a01b447"
-      };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
-      console.log('this working');
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    database = firebase.database();
+    console.log('this working');
+    //firebase authentication stuff:
+    $("#sign-in").on("click", function (event) {
+        event.preventDefault();
+        var email = $("#email").val().trim();
+        var password = $("#password").val().trim();
+        console.log(email, password);
+        if (!email || !password) {
+            $("#sign-in-message").text("please input both email and password to sign in, or create one by registering an account.");
+        } else {
+            firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+                $("#sign-in-message").text(error.message);
+            });
+        }
+    })
+
+    $("#register").on("click", function (event) {
+        event.preventDefault();
+        var email = $("#email").val().trim();
+        var password = $("#password").val().trim();
+        console.log(email, password);
+        if (!email || !password) {
+            $("#sign-in-message").text("please input both email and password to sign in, or create one by registering an account.");
+        } else {
+            firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+                $("#sign-in-message").text(error.message);
+            });
+        }
+    })
 
 
+    $(".sign-out").on("click", function () {
+        firebase.auth().signOut();
+    })
 
 
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
 
+            console.log(user);
+            $("#sign-in-wrapper").css("display", "none");
+            $("#app-wrapper").css("display", "block");
+        } else {
+            console.log("test");
+            $("#sign-in-wrapper").css("display", "block");
+            $("#app-wrapper").css("display", "none");
 
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    })
 
 
 
@@ -48,3 +72,5 @@ $(document).ready(function(){
 
 
 })
+
+   
