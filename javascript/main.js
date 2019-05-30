563251113c137c7d852988cdb5df8b07b4c97c1b
Matt D <dwinell.matt@gmail.com>
2019-05-30 10:29:03 -0400

    linked files

---
+++ javascript/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    var firebaseConfig = {
+        apiKey: "AIzaSyCfx7JuJ4eEZIpbbgNmvNPTOkPUDuIVyyo",
+        authDomain: "dndcharcreator-2c7a2.firebaseapp.com",
+        databaseURL: "https://dndcharcreator-2c7a2.firebaseio.com",
+        projectId: "dndcharcreator-2c7a2",
+        storageBucket: "dndcharcreator-2c7a2.appspot.com",
+        messagingSenderId: "427842856156",
+        appId: "1:427842856156:web:71475bda7a01b447"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      console.log('this working');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
