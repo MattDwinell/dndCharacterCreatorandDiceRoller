cec9498b874a520229590c696ecc585bf8687970
Matt D <dwinell.matt@gmail.com>
2019-05-31 15:11:17 -0400

    added option to roll multiple of one dice type at a time

--- javascript/main.js
+++ javascript/main.js
@@ -50,17 +50,35 @@ $(document).ready(function () {
 
     function rollDie(element) {
         showDie();
+        var numOfDice = parseInt($("#num-of-dice").val().trim());
+        console.log(numOfDice);
+        if ( !numOfDice){
+            numOfDice = 1;
+        }
+        console.log(numOfDice);
+        document.getElementById('num-of-dice').value = 1;
+        console.log(numOfDice);
         var diceSize = element.target.id;
         var diceNum = parseFloat(diceSize.substr(1));
         console.log(diceNum);
         var dieRoll = Math.ceil(Math.random() * diceNum);
         console.log(dieRoll);
-        rollDisplay(diceSize, dieRoll);
+        rollDisplay(diceNum, dieRoll, numOfDice);
     }
 
-    function rollDisplay(maxNum, ActualNum) {
-        var rollMessage = $("<p>").text('rolled a(n) ' + ActualNum + ' using a ' + maxNum).attr("class", "roll-message");
+    function rollDisplay(maxNum, ActualNum, numDice) {
+        console.log(maxNum,ActualNum, numDice)
+        if(numDice == 1 || numDice == 0){
+        var rollMessage = $("<p>").text('rolled a(n) ' + ActualNum + ' using a d' + maxNum).attr("class", "roll-message");
         $("#roll-holder").prepend(rollMessage);
+        } else {
+            for(let i=1; i<numDice; i++){
+                ActualNum += Math.ceil(Math.random()*maxNum);
+            }
+            console.log(ActualNum);
+            var rollMessage = $("<p>").text('rolled a(n) ' + ActualNum + ' using ' + numDice  + " d" + maxNum +"s").attr("class", "roll-message");
+        $("#roll-holder").prepend(rollMessage);
+        }
         messageCount++;
         rollScrubber();
     }
